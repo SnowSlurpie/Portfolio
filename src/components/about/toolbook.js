f322beb69afe93a6ac6fdab1d5c2d9329ebb9590
Alex Running <runniale000@gmail.com>
2022-07-17 21:25:40 -0500

    created toolbook

---
+++ src/components/about/toolbook.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiHeroku,
+} from "react-icons/si";
+
+function toolbook() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+    </Row>
+  );
+}
+
+export default toolbook;
